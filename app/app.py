8da9bceaf9e68619fb7558e4e6e247f551aeb552
myrmica-habilis <michalportes1@gmail.com>
2020-11-04 21:39:59 +0100

    Use h1 tag, update view for home

--- app/app.py
+++ app/app.py
@@ -10,22 +10,23 @@ def home():
     return """
     <a href="date">date</a>
     <a href="time">time</a>
+    <a href="date_time">date & time</a>
     """
 
 
 @bottle.route('/date')
 def date():
-    return datetime.now().strftime('%x')
+    return f'<h1>{datetime.now().strftime("%x")}</h1>'
 
 
 @bottle.route('/time')
 def time():
-    return datetime.now().strftime('%X')
+    return f'<h1>{datetime.now().strftime("%X")}</h1>'
 
 
 @bottle.route('/date_time')
 def date_time():
-    return datetime.now().strftime('%c')
+    return f'<h1>{datetime.now().strftime("%c")}</h1>'
 
 
 application = bottle.default_app()
