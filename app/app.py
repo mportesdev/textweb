aea19aad8eeeb2e35ceb6dc330c02a91360c32f5
myrmica-habilis <michalportes1@gmail.com>
2020-11-04 21:50:00 +0100

    Reformat html content

--- app/app.py
+++ app/app.py
@@ -5,32 +5,44 @@ from bottle import route, default_app
 
 @route('/')
 def home():
-    return """
-    <p>
-    <a href="date">date</a>
-    </p>
-    <p>
-    <a href="time">time</a>
-    </p>
-    <p>
-    <a href="date_time">date & time</a>
-    </p>
-    """
+    return '''
+        <p>
+            <a href="date">date</a>
+        </p>
+        <p>
+            <a href="time">time</a>
+        </p>
+        <p>
+            <a href="date_time">date & time</a>
+        </p>
+    '''
 
 
 @route('/date')
 def date():
-    return f'<h1>{datetime.now().strftime("%x")}</h1>'
+    return f'''
+        <h1>
+            {datetime.now().strftime("%x")}
+        </h1>
+    '''
 
 
 @route('/time')
 def time():
-    return f'<h1>{datetime.now().strftime("%X")}</h1>'
+    return f'''
+        <h1>
+            {datetime.now().strftime("%X")}
+        </h1>
+    '''
 
 
 @route('/date_time')
 def date_time():
-    return f'<h1>{datetime.now().strftime("%c")}</h1>'
+    return f'''
+        <h1>
+            {datetime.now().strftime("%c")}
+        </h1>
+    '''
 
 
 application = default_app()
