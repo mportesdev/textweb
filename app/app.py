feca2caaf3c87f9948cc165920ea6e7c52017647
myrmica-habilis <michalportes1@gmail.com>
2020-11-05 17:52:14 +0100

    Change formats

--- app/app.py
+++ app/app.py
@@ -21,9 +21,9 @@ def home():
 @route('/<name:re:date|time|date_time>')
 def date_time(name):
     format_spec = {
-        'date': '%x',
-        'time': '%X',
-        'date_time': '%c',
+        'date': '%-d. %-m. %Y',
+        'time': '%-H:%M:%S',
+        'date_time': '%-d. %-m. %Y, %-H:%M:%S',
     }.get(name)
     text = datetime.now().strftime(format_spec)
     return f'<h1>{text}</h1>'
