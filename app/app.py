55a77621f33451de41a17abdd51dc5ddda402fca
myrmica-habilis <michalportes1@gmail.com>
2020-11-10 10:13:13 +0100

    Generate page title

--- app/app.py
+++ app/app.py
@@ -26,20 +26,22 @@ def home():
 @application.route('/<name:re:date|time|date_time>')
 @view('simple')
 def date_time(name):
+    title = name.replace('_', ' & ').title()
     format_spec = {
         'date': '%-d. %-m. %Y',
         'time': '%-H:%M:%S',
         'date_time': '%-d. %-m. %Y, %-H:%M:%S',
     }.get(name)
     text = datetime.now().strftime(format_spec)
-    return {'text': text}
+    return {'title': title, 'text': text}
 
 
 @application.route('/oli/obrazky')
 @view('gallery')
 def oli_gallery():
+    title = 'Oli maluje'
     pictures = [0, 1, 2]
-    return {'pictures': pictures}
+    return {'title': title, 'pictures': pictures}
 
 
 @application.route('/oli/obrazky/<picture_id:int>')
