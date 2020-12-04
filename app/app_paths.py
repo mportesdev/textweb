b104341920104e5ecdb86720df999ee89a55db84
myrmica-habilis <michalportes1@gmail.com>
2020-12-04 12:22:37 +0100

    Add database

--- app/app_paths.py
+++ app/app_paths.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
-STATIC_PATH = Path(__file__).resolve().parent / 'static'
+APP_PATH = Path(__file__).resolve().parent
+
+DB_PATH = APP_PATH / 'db' / 'db.sqlite3'
+STATIC_PATH = APP_PATH / 'static'
 IMG_PATH = STATIC_PATH / 'img'
