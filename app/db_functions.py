05a6eb18704482d5e47ee96f4ffdfe3e709f192c
myrmica-habilis <michalportes1@gmail.com>
2021-02-10 10:09:59 +0100

    Refactor querying db (DRY)

--- app/db_functions.py
+++ app/db_functions.py
@@ -3,30 +3,29 @@ import sqlite3
 from app_paths import DB_PATH
 
 
-def get_person_names():
+def query_db(sql_query, params=()):
     con = sqlite3.connect(DB_PATH)
     con.row_factory = sqlite3.Row
-
     cur = con.cursor()
-    for row in cur.execute('SELECT name FROM Person'):
-        yield row['name']
-
+    yield from cur.execute(sql_query, params)
     con.close()
+
+
+def get_person_names():
+    sql = 'SELECT name FROM Person'
+    for row in query_db(sql):
+        yield row['name']
 
 
 def get_meter_data(name):
-    con = sqlite3.connect(DB_PATH)
-    con.row_factory = sqlite3.Row
-
-    cur = con.cursor()
-    for row in cur.execute('SELECT Meter.date, Meter.height '
-                           'FROM Person '
-                           'INNER JOIN Meter ON Meter.person=Person.id '
-                           'WHERE Person.name=?',
-                           (name,)):
+    sql = '''
+        SELECT Meter.date, Meter.height 
+        FROM Person 
+        INNER JOIN Meter ON Meter.person=Person.id 
+        WHERE Person.name=?
+    '''
+    for row in query_db(sql, (name,)):
         yield dict(row)
-
-    con.close()
 
 
 def entries_exist(table_name, name):
